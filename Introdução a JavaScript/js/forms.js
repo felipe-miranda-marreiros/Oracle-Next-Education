f3a969b260c641dd229e845f46072dc85ee0e99d
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-01-08 20:53:10 -0300

    [Projeto IMC]: Criando funções para incluir Objetos

--- Introdução a JavaScript/js/forms.js
+++ Introdução a JavaScript/js/forms.js
@@ -7,32 +7,14 @@ botaoAdicionar.addEventListener("click", function (event) {
 
   const paciente = obtemPacienteDoFormulario(form);
 
-  const pacienteTr = document.createElement("tr");
-
-  const nomeTd = document.createElement("td");
-  const pesoTd = document.createElement("td");
-  const alturaTd = document.createElement("td");
-  const gorduraTd = document.createElement("td");
-  const imcTd = document.createElement("td");
-
-  nomeTd.textContent = nome;
-  pesoTd.textContent = peso;
-  alturaTd.textContent = form;
-  gorduraTd.textContent = gordura;
-  imcTd.textContent = calcularIMC(peso, altura);
-
-  pacienteTr.appendChild(nomeTd);
-  pacienteTr.appendChild(pesoTd);
-  pacienteTr.appendChild(alturaTd);
-  pacienteTr.appendChild(gorduraTd);
-  pacienteTr.appendChild(imcTd);
-
+  const pacienteTr = montaTr(paciente);
+  
   const tabela = document.querySelector("#tabela-pacientes");
 
   tabela.appendChild(pacienteTr);
 });
 
-function obtemPacienteDoFormulario (form) {
+function obtemPacienteDoFormulario(form) {
   const paciente = {
     nome: form.nome.value,
     peso: form.peso.value,
@@ -42,4 +24,28 @@ function obtemPacienteDoFormulario (form) {
   };
 
   return paciente;
+}
+
+function montaTr(paciente) {
+  const pacienteTr = document.createElement("tr");
+
+  const nomeTd = document.createElement("td");
+  const pesoTd = document.createElement("td");
+  const alturaTd = document.createElement("td");
+  const gorduraTd = document.createElement("td");
+  const imcTd = document.createElement("td");
+
+  nomeTd.textContent = paciente.nome;
+  pesoTd.textContent = paciente.peso;
+  alturaTd.textContent = paciente.altura;
+  gorduraTd.textContent = paciente.gordura;
+  imcTd.textContent = paciente.imc;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(imcTd);
+
+  return pacienteTr;
 }
