961e13c826143983740666cbfdc09c0e87937935
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-01-08 18:27:02 -0300

    [Projeto IMC]: Manipulando CSS com JavaScript

--- Introdução a JavaScript/js/calcular-IMC.js
+++ Introdução a JavaScript/js/calcular-IMC.js
@@ -1,28 +1,31 @@
 const pacientes = document.querySelectorAll(".paciente");
 
-for (var i = 0; i < pacientes.length; i++) {
-
+for (let i = 0; i < pacientes.length; i++) {
   const paciente = pacientes[i];
 
   const tdPeso = paciente.querySelector(".info-peso");
-  const peso = tdPeso.textContent;
+  let peso = tdPeso.textContent;
 
   const tdAltura = paciente.querySelector(".info-altura");
   const altura = tdAltura.textContent;
 
   const tdImc = paciente.querySelector(".info-imc");
 
-  const pesoEhValido = true;
-  const alturaEhValida = true;
+  let pesoEhValido = true;
+  let alturaEhValida = true;
 
   if (peso <= 0 || peso >= 1000) {
     pesoEhValido = false;
     tdImc.textContent = "Peso inválido";
+
+    paciente.classList.add("paciente-invalido");
   }
 
   if (altura <= 0 || altura >= 3.0) {
     alturaEhValida = false;
     tdImc.textContent = "Altura inválida";
+
+    paciente.classList.add("paciente-invalido");
   }
 
   if (alturaEhValida && pesoEhValido) {
