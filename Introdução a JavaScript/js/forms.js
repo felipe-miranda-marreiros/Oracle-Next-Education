aafbdee4ffe67a64a52e4d08a1944314e2ed7c06
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-01-08 18:44:40 -0300

    [Projeto IMC]: Incorporando Forms no JavaScript

---
+++ Introdução a JavaScript/js/forms.js
@@ -0,0 +1,35 @@
+const botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const form = document.querySelector("#form-adiciona");
+
+  const nome = form.nome.value;
+  const peso = form.peso.value;
+  const altura = form.altura.value;
+  const gordura = form.gordura.value;
+
+  const pacienteTr = document.createElement("tr");
+
+  const nomeTd = document.createElement("td");
+  const pesoTd = document.createElement("td");
+  const alturaTd = document.createElement("td");
+  const gorduraTd = document.createElement("td");
+  const imcTd = document.createElement("td");
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+  imcTd.textContent = imcTd;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+
+  const tabela = document.querySelector("#tabela-pacientes");
+
+  tabela.appendChild(pacienteTr);
+});
