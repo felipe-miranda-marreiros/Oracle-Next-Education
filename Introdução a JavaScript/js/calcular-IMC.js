43d663631d965f7652e8553cab17714c9b82155c
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-01-08 21:35:58 -0300

    [Projeto IMC]: Função para validação de peso e altura

--- Introdução a JavaScript/js/calcular-IMC.js
+++ Introdução a JavaScript/js/calcular-IMC.js
@@ -11,17 +11,17 @@ for (let i = 0; i < pacientes.length; i++) {
 
   const tdImc = paciente.querySelector(".info-imc");
 
-  let pesoEhValido = true;
-  let alturaEhValida = true;
+  let pesoEhValido = validaPeso(peso);
+  let alturaEhValida = validaAltura(altura);
 
-  if (peso <= 0 || peso >= 1000) {
+  if (!pesoEhValido) {
     pesoEhValido = false;
     tdImc.textContent = "Peso inválido";
 
     paciente.classList.add("paciente-invalido");
   }
 
-  if (altura <= 0 || altura >= 3.0) {
+  if (!alturaEhValida) {
     alturaEhValida = false;
     tdImc.textContent = "Altura inválida";
 
@@ -35,8 +35,22 @@ for (let i = 0; i < pacientes.length; i++) {
 }
 
 function calcularIMC(peso, altura) {
-
   let imc = 0;
   imc = peso / (altura * altura);
   return imc.toFixed(2);
+}
+
+function validaPeso(peso) {
+  if (peso >= 0 && peso <= 100) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function validaAltura(altura) {
+  if (altura >= 0 && altura <= 3.0) {
+    return true;
+  } else {
+    return false;
+  }
 }
