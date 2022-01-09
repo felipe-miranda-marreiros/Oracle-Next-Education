bae9cf7a960228f14af1e62134f19b44470fea8f
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-01-09 11:06:27 -0300

    [Projeto IMC]: Filtragem com expressão regular

--- Introdução a JavaScript/js/filtrar.js
+++ Introdução a JavaScript/js/filtrar.js
@@ -1,22 +1,24 @@
 const campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function () {
-  const paciente = document.querySelectorAll(".paciente");
+  const pacientes = document.querySelectorAll(".paciente");
 
   if (this.value.length > 0) {
-    for (let i = 0; i < paciente.length; i++) {
+    for (let i = 0; i < pacientes.length; i++) {
       let paciente = pacientes[i];
       let tdNome = paciente.querySelector(".info-nome");
       let nome = tdNome.textContent;
 
-      if (nome != this.value) {
+      let expressao = new RegExp(this.value, "i");
+
+      if (!expressao.test(nome)) {
         paciente.classList.add("invisivel");
       } else {
         paciente.classList.remove("invisivel");
       }
     }
   } else {
-    for (let i = 0; i < paciente.length; i++) {
+    for (let i = 0; i < pacientes.length; i++) {
       let paciente = pacientes[i];
       paciente.classList.remove("invisivel");
     }
