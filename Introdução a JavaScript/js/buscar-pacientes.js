9cc1705e02d109d86d87847e9cdf0c3dc1a5c2cf
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-01-09 15:39:20 -0300

    [Projeto IMC]: Capturando erros de requisição

--- Introdução a JavaScript/js/buscar-pacientes.js
+++ Introdução a JavaScript/js/buscar-pacientes.js
@@ -3,14 +3,19 @@ const botaoBuscarPacientes = document.querySelector("#buscar-pacientes");
 botaoBuscarPacientes.addEventListener("click", function () {
   const xhr = new XMLHttpRequest();
 
-  xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+  xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes1");
 
   xhr.addEventListener("load", function () {
-    const resposta = xhr.responseText;
-    const pacientes = JSON.parse(resposta);
-    pacientes.forEach((pacientes) => {
-      adicionaPacienteNaTela(pacientes);
-    });
+    if (xhr.status == 200) {
+      const resposta = xhr.responseText;
+      const pacientes = JSON.parse(resposta);
+      pacientes.forEach((pacientes) => {
+        adicionaPacienteNaTela(pacientes);
+      });
+    } else {
+      const erroAjax = document.querySelector("#erro-ajax");
+      erroAjax.classList.toggle("invisivel");
+    }
   });
   xhr.send();
 });
